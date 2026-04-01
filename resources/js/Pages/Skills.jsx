d9bac6039d660519ec0bb0f3684bbb4cd40244f8
maikl2l <maikl2l29@gmail.com>
2026-04-01 16:12:04 +0300

    skills styles

--- resources/js/Pages/Skills.jsx
+++ resources/js/Pages/Skills.jsx
@@ -123,45 +123,27 @@ function SkillCard({ skill, index }) {
             onMouseLeave={e => e.currentTarget.style.boxShadow = '0 0 0 0 transparent'}
         >
             {/* Top row: icon + level badge */}
-            <div className="flex items-start justify-between mb-4">
-                <div
-                    className="flex h-9 w-9 items-center justify-center rounded-lg bg-gray-100 text-base text-gray-700 group-hover:bg-indigo-500/10 group-hover:text-indigo-600 transition-all duration-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32" className="w-10 h-10">
-                        <circle cx="16" cy="16" r="14" fill="#8892BF"></circle>
-                        <path fill="#fff"
-                              d="M14.44 10h1.68l-.476 2.524h1.51q1.242.027 1.85.555.621.529.366 2.01l-.816 4.4H16.85l.779-4.203q.121-.66-.073-.938-.195-.278-.84-.278l-1.352-.013-.998 5.432h-1.68z"></path>
-                        <path fill="#fff" fill-rule="evenodd"
-                              d="M6.74 12.524h3.264q1.436.014 2.081.899.646.885.426 2.418a5 5 0 0 1-.377 1.375q-.28.674-.779 1.216-.609.687-1.303.872a5.6 5.6 0 0 1-1.436.185H7.155L6.692 22H5zm.695 5.472.728-3.952h.246q.134 0 .28-.013.975-.014 1.619.198.657.211.438 1.6-.267 1.65-1.047 1.929-.779.264-1.948.25h-.17a1 1 0 0 1-.146-.012M24.437 12.524h-3.263L19.433 22h1.692l.463-2.511h1.46a5.6 5.6 0 0 0 1.437-.185q.694-.185 1.303-.872.499-.543.779-1.216.292-.675.377-1.375.22-1.533-.426-2.418-.645-.885-2.082-.899m-1.841 1.52-.728 3.952a1 1 0 0 0 .146.013h.17q1.17.013 1.948-.251.78-.277 1.047-1.93.219-1.387-.438-1.599-.645-.21-1.62-.198a3 3 0 0 1-.28.013z"
-                              clip-rule="evenodd"></path>
-                    </svg>
+            <div className="grid grid-flow-col justify-items-center mb-4">
+                <div className="flex flex-col">
+                    <div
+                        className="flex h-9 w-9 items-center justify-center rounded-lg bg-gray-100 text-base text-gray-700 group-hover:bg-indigo-500/10 group-hover:text-indigo-600 transition-all duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32" className="w-10 h-10">
+                            <circle cx="16" cy="16" r="14" fill="#8892BF"></circle>
+                            <path fill="#fff"
+                                  d="M14.44 10h1.68l-.476 2.524h1.51q1.242.027 1.85.555.621.529.366 2.01l-.816 4.4H16.85l.779-4.203q.121-.66-.073-.938-.195-.278-.84-.278l-1.352-.013-.998 5.432h-1.68z"></path>
+                            <path fill="#fff" fill-rule="evenodd"
+                                  d="M6.74 12.524h3.264q1.436.014 2.081.899.646.885.426 2.418a5 5 0 0 1-.377 1.375q-.28.674-.779 1.216-.609.687-1.303.872a5.6 5.6 0 0 1-1.436.185H7.155L6.692 22H5zm.695 5.472.728-3.952h.246q.134 0 .28-.013.975-.014 1.619.198.657.211.438 1.6-.267 1.65-1.047 1.929-.779.264-1.948.25h-.17a1 1 0 0 1-.146-.012M24.437 12.524h-3.263L19.433 22h1.692l.463-2.511h1.46a5.6 5.6 0 0 0 1.437-.185q.694-.185 1.303-.872.499-.543.779-1.216.292-.675.377-1.375.22-1.533-.426-2.418-.645-.885-2.082-.899m-1.841 1.52-.728 3.952a1 1 0 0 0 .146.013h.17q1.17.013 1.948-.251.78-.277 1.047-1.93.219-1.387-.438-1.599-.645-.21-1.62-.198a3 3 0 0 1-.28.013z"
+                                  clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <div className="w-9 h-1 bg-indigo-500 rounded-full mt-2"></div>
                 </div>
-                <span
-                    className={`text-[10px] font-mono font-medium px-2 py-1 rounded-full ${LEVEL_STYLES[skill.level] ?? LEVEL_STYLES.Mid}`}>
-                    {skill.level}
-                </span>
             </div>
 
             {/* Name */}
             <h3 className="mb-1.5 text-sm font-semibold tracking-tight text-gray-900 group-hover:text-black transition-colors duration-200">
                 {skill.name}
             </h3>
-
-            {/* Description */}
-            <p className="mb-4 text-xs leading-relaxed text-gray-500 group-hover:text-gray-700 transition-colors duration-200">
-                {skill.description}
-            </p>
-
-            {/* Tags */}
-            <div className="w-full flex flex-wrap gap-1.5">
-                {(skill.tags ?? []).map(tag => (
-                    <span
-                        key={tag}
-                        className="font-mono text-[10px] px-2 py-0.5 rounded-md bg-gray-100 text-gray-600 border border-gray-200 group-hover:border-gray-300 group-hover:text-gray-800 transition-all duration-200"
-                    >
-                        {tag}
-                    </span>
-                ))}
-            </div>
         </div>
     );
 }
