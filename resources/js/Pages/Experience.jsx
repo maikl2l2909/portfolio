ae5ff56f2cf44e074d78c42de964339712b36653
maikl2l <maikl2l29@gmail.com>
2026-06-16 16:06:56 +0300

    adapted to mobile version the experience page

--- resources/js/Pages/Experience.jsx
+++ resources/js/Pages/Experience.jsx
@@ -34,7 +34,7 @@ function ExperienceItem({ experience, number, formatMonthYear, presentLabel }) {
     >
       <div>
         <div className={`flex flex-col items-center gap-8 ${number % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
-          <div className="absolute left-4 md:left-1/2 md:-translate-x-1/2 top-0 w-8 h-8 rounded-full bg-white border-2 border-red-600 flex items-center justify-center z-10 shadow-[0_0_15px_rgba(220,38,38,0.5)]">
+          <div className="hidden md:flex absolute left-4 md:left-1/2 md:-translate-x-1/2 top-0 w-8 h-8 rounded-full bg-white border-2 border-red-600 items-center justify-center z-10 shadow-[0_0_15px_rgba(220,38,38,0.5)]">
             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" className="text-red-600 w-4 h-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
               <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"></path>
               <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.4 5.4 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2z"></path>
@@ -141,7 +141,7 @@ export default function Experience({ experiences = [], sectionId, embedded = fal
           </p>
         ) : (
           <div className="relative">
-            <div className="absolute left-4 md:left-1/2 md:-translate-x-1/2 top-0 bottom-0 w-[2px] bg-gradient-to-b from-red-600 via-red-950 to-transparent opacity-30"></div>
+            <div className="hidden md:block absolute left-4 md:left-1/2 md:-translate-x-1/2 top-0 bottom-0 w-[2px] bg-gradient-to-b from-red-600 via-red-950 to-transparent opacity-30"></div>
             <div className="flex flex-col gap-16 lg:gap-24">
               {experiences.map((experience, number) => (
                 <ExperienceItem
